6cd640e1c7b33dc5d4f9cd667135e078ebe526d5
Jangboo Lee <github@jangboolee.com>
2025-08-02 17:17:42 +0200

    Create methods to load and read tables

--- src/db/db_handler.py
+++ src/db/db_handler.py
@@ -1,7 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, configure_mappers, sessionmaker
+from pathlib import Path
 
-from src.db.schema import Base
+from pandas import read_csv
+from sqlalchemy import Select, create_engine, insert, select
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Session,
+    configure_mappers,
+    sessionmaker,
+)
+
+from src.db.schema import Base, City
 
 
 class DBHandler:
@@ -9,7 +17,11 @@ class DBHandler:
         self.uri = "sqlite:///data/pararius_scrape.db"
         self.engine = create_engine(self.uri)
         self.Session = sessionmaker(bind=self.engine)
+        self.orms = {City: Path(".") / "data" / "city.csv"}
         self._create_all()
+
+    def get_session(self) -> Session:
+        return self.Session()
 
     def _create_all(self) -> bool:
         Base.metadata.create_all(self.engine)
@@ -20,8 +32,27 @@ class DBHandler:
         Base.metadata.drop_all(self.engine)
         return True
 
-    def reset(self) -> bool:
-        return all([self._drop_all(), self._create_all()])
+    def _load_tables(self) -> bool:
+        for orm, file_path in self.orms.items():
+            # Read the CSV file as a dataframe
+            df = read_csv(file_path)
+            # Write contents of the dataframe into the DB
+            with self.get_session() as session:
+                rows = df.to_dict(orient="records")
+                session.execute(
+                    insert(orm),
+                    rows,
+                )
+                session.commit()
 
-    def get_session(self) -> Session:
-        return self.Session()
+        return True
+
+    def reset(self) -> bool:
+        return all([self._drop_all(), self._create_all(), self._load_tables()])
+
+    def read_table(
+        self, orm: DeclarativeBase, stmt: Select | None = None
+    ) -> list[DeclarativeBase]:
+        with self.get_session() as session:
+            stmt = stmt if stmt is not None else select(orm)
+            return session.scalars(stmt).all()
